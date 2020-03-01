514fd3ec78a3aff92dc8204bcf3309ba49fd33b0
Naim Aryudya Azizi <naimaryudya@gmail.com>
2020-03-01 15:18:11 +0700

    add request module to consume api

--- app/models/response.py
+++ app/models/response.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Any
 
 
 class DatetimeValue(BaseModel):
@@ -10,3 +11,17 @@ class DatetimeValue(BaseModel):
 class CategoricalValue(BaseModel):
     category: str
     value: int = 0
+
+
+class ResponseModel():
+    def __init__(self, status_code: str, data: Any):
+        self.status_code = status_code
+        self.data = data
+
+    @property
+    def data(self):
+        return self.data
+
+    @property
+    def status_code(self):
+        return self.status_code
---
+++ app/services/request.py
@@ -0,0 +1,84 @@
+import urllib3
+from urllib3.exceptions import EmptyPoolError, TimeoutError, PoolError
+from typing import Optional, Dict
+from loguru import logger
+import json
+
+from app.models.response import ResponseModel
+
+
+class Request():
+    _instance = None
+    _req = None
+
+    @classmethod
+    def config(cls, num_pools: int = 10, header: str = None, **kwargs) -> None:
+        cls._req = urllib3.PoolManager(num_pools, header, **kwargs)
+
+    @classmethod
+    def get(
+            cls, url: str, header: str = None, is_json_response: bool = True
+            ) -> Optional[ResponseModel]:
+
+        response = None
+        try:
+            res = cls._req.request('GET', url, headers=header)
+            if is_json_response:
+                response = ResponseModel(
+                    res.status, json.loads(res.data.decode('utf-8')))
+            else:
+                response = ResponseModel(
+                    res.status, res.data)
+        except TypeError:
+            logger.opt(exception=True).error(
+                'Response could not be parsed into JSON.\nResponse:\n{}',
+                res.data)
+        except (EmptyPoolError, PoolError):
+            logger.opt(exception=True).error(
+                'Max http connection pool is reached')
+        except TimeoutError:
+            logger.opt(exception=True).error(
+                'Timeout in http request to url:{}', url)
+        except Exception:
+            logger.opt(exception=True).error(
+                'Error in creating http request to url:{}', url)
+        return response
+
+    @classmethod
+    def post(
+            cls, url: str, fields: Dict = None,
+            header: str = None, is_json_response: bool = True,
+            ) -> Optional[ResponseModel]:
+
+        response = None
+        try:
+            res = cls._req.request('POST', url, fields=fields, headers=header)
+            if is_json_response:
+                response = ResponseModel(
+                    res.status, json.loads(res.data.decode('utf-8')))
+            else:
+                response = ResponseModel(
+                    res.status, res.data)
+        except TypeError:
+            logger.opt(exception=True).error(
+                'Response could not be parsed into JSON.\nResponse:\n{}',
+                res.data)
+        except (EmptyPoolError, PoolError):
+            logger.opt(exception=True).error(
+                'Max http connection pool is reached')
+        except TimeoutError:
+            logger.opt(exception=True).error(
+                'Timeout in http request to url:{}', url)
+        except Exception:
+            logger.opt(exception=True).error(
+                'Error in creating http request to url:{}', url)
+        return response
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Request, cls).__new__(cls)
+            cls._req = urllib3.PoolManager(num_pools=10)
+        return cls._instance
+
+
+request = Request()
